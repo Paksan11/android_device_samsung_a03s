eebfb81605fbe48541e2d6350d68cb896f430758
Paksan11 <87140907+Paksan11@users.noreply.github.com>
2024-12-10 21:54:30 +0700

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,10 +144,11 @@ TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_SECONDARY_BRIGHTNESS_PATH := "/sys/class/leds/vibrator/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 200
 TW_USE_SAMSUNG_HAPTICS := true
-# Remove these flash when building SHRP
 TW_Y_OFFSET := 80
 TW_H_OFFSET := -80
 
